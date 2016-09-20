d9a8b729c8aa329746af1aca1ccb21540f798daa
Boria7777 <boria7777@outlook.com>
2016-09-20 14:01:10 +0800

    First Commit

---
+++ app/src/main/java/com/example/smoothimagedemo/SpaceImageDetailActivity.java
@@ -0,0 +1,75 @@
+package com.example.smoothimagedemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView.ScaleType;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.roamer.ui.view.SmoothImageView;
+
+public class SpaceImageDetailActivity extends Activity {
+
+    private int mLocationX;
+    private int mLocationY;
+    private int mWidth;
+    private int mHeight;
+    SmoothImageView imageView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//		mDatas = (ArrayList<String>) getIntent().getSerializableExtra("images");
+//		mPosition = getIntent().getIntExtra("position", 0);
+        mLocationX = getIntent().getIntExtra("locationX", 0);
+        mLocationY = getIntent().getIntExtra("locationY", 0);
+        mWidth = getIntent().getIntExtra("width", 0);
+        mHeight = getIntent().getIntExtra("height", 0);
+
+        imageView = new SmoothImageView(this);
+        imageView.setOriginalInfo(mWidth, mHeight, mLocationX, mLocationY);
+        imageView.transformIn();
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(-1, -1));
+        imageView.setScaleType(ScaleType.FIT_CENTER);
+        setContentView(imageView);
+        ImageLoader.getInstance().displayImage("http://hiphotos.baidu.com/%B3%F5%BC%B6%BE%D1%BB%F7%CA%D6/pic/item/929b56443840bfc6b3b7dc64.jpg", imageView);
+//		imageView.setImageResource(R.drawable.temp);
+        // ScaleAnimation scaleAnimation = new ScaleAnimation(0.5f, 1.0f, 0.5f,
+        // 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+        // 0.5f);
+        // scaleAnimation.setDuration(300);
+        // scaleAnimation.setInterpolator(new AccelerateInterpolator());
+        // imageView.startAnimation(scaleAnimation);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        imageView.setOnTransformListener(new SmoothImageView.TransformListener() {
+            @Override
+            public void onTransformComplete(int mode) {
+                if (mode == 2) {
+                    finish();
+                }
+            }
+        });
+        imageView.transformOut();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isFinishing()) {
+            overridePendingTransition(0, 0);
+        }
+    }
+
+}
